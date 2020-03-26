d58465d4fd4c2ed863168d3dc1199b928fd10384
My Lilja <my.s.lilja@gmail.com>
2020-03-26 12:52:19 +0100

    Created BackgroundButtons
    Created buttons with images of the backgrounds

---
+++ Puzzle jigsaw/Backgrounds.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Puzzle_jigsaw
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Backgrounds : Window
+    {
+        public Backgrounds()
+        {
+            InitializeComponent();
+        }
+
+        private void Leaf_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
