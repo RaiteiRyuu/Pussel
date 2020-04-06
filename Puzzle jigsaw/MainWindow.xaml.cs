8ea123fcc12b4cc001de1186e7595f2f4aaf418f
Ryuu <evil__dark__angel@hotmail.com>
2020-04-06 19:59:56 +0200

    cutecat
    added Cute_Cat as main picture

--- Puzzle jigsaw/MainWindow.xaml.cs
+++ Puzzle jigsaw/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace Puzzle_jigsaw
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int counter = 0;
         private Backgrounds backgroundCombobox = null;
         private FullImage popupFullImageWindow = null;
         private Puzzle_Pieces popupPuzzlePiecesWindow = null;
@@ -79,68 +80,75 @@ namespace Puzzle_jigsaw
         
         private void onclick(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog open_File = new OpenFileDialog();
-            open_File.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
-            if (open_File.ShowDialog() == true)
-            {
+            Image img = new Image();
+            //img.SetValue(Canvas.ZIndexProperty, 0);
+            img.Source = new BitmapImage(new Uri("Image/Cute_Cat.jpg", UriKind.Relative));
+            img.Width = imageCanvas.Width;
+            img.Height = imageCanvas.Height;
+            imageCanvas.Children.Add(img);
+            popupFullImageWindow.FullImageImage.Source = img.Source;
+            //OpenFileDialog open_File = new OpenFileDialog();
+            //open_File.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
+            //if (open_File.ShowDialog() == true)
+            //{
 
-                BitmapImage img = new BitmapImage(new Uri(open_File.FileName));
-                //imgPhoto.Source = img;
-                popupFullImageWindow.FullImageImage.Source = img;
+            //    BitmapImage img = new BitmapImage(new Uri(open_File.FileName));
+            //    //imgPhoto.Source = img;
+            //    //popupFullImageWindow.FullImageImage.Source = img;
 
-                Image[,] image = new Image[4, 4];
-                for (int x = 0; x < 4; x++)
-                {
-               
-                    for (int y = 0; y < 4; y++)
-                    {
-                       
-                            image[x, y] = new Image();
-                            image[x, y].Width = 200;
-                            image[x, y].Height = 95;
-                            //image[x, y].Name = $"cb_x{x}_y{y}";
-                            image[x, y].HorizontalAlignment = HorizontalAlignment.Left;
-                            image[x, y].VerticalAlignment = VerticalAlignment.Top;
-                            image[x, y].Margin = new Thickness(x * 96, y * 96, 0, 0);
-                            PuzzleGrid.Children.Add(image[x, y]);
-                            Console.WriteLine(PuzzleGrid);
-                    }
-                }
-                CroppedBitmap cb;
-                for (int x = 0; x < 4; x++)
-                {
+            //    Image[,] image = new Image[4, 4];
+            //    for (int x = 0; x < 4; x++)
+            //    {
 
-                    for (int y = 0; y < 4; y++)
-                    {
-                                //JU STÖRRE BILD. DESTO HÖGRE SKALA OCH ANTAL PIXLAR I "Int32Rect"
-                            cb = new CroppedBitmap(img, new Int32Rect(x * 200, y * 200, 200,200));
+            //        for (int y = 0; y < 4; y++)
+            //        {
 
-                        
-                        Random pix = new Random();
-                        image[x, y].Source = cb;
-                    }
-                }
+            //                image[x, y] = new Image();
+            //                image[x, y].Width = 200;
+            //                image[x, y].Height = 95;
+            //                //image[x, y].Name = $"cb_x{x}_y{y}";
+            //                image[x, y].HorizontalAlignment = HorizontalAlignment.Left;
+            //                image[x, y].VerticalAlignment = VerticalAlignment.Top;
+            //                image[x, y].Margin = new Thickness(x * 96, y * 96, 0, 0);
+            //                PuzzleGrid.Children.Add(image[x, y]);
+            //                Console.WriteLine(PuzzleGrid);
+            //        }
+            //    }
+            //    CroppedBitmap cb;
+            //    for (int x = 0; x < 4; x++)
+            //    {
 
-
-                ////BitmapImage bitmap = new BitmapImage();
-                ////bitmap.BeginInit();
-                //////bitmap.UriSource = new Uri(open_File.FileName);
-                ////bitmap.EndInit();
-
-                //// Create a CroppedBitmap from BitmapImage  
-                //CroppedBitmap cbit1 = new CroppedBitmap(img, new Int32Rect(0, 0, 250, 200));
-                //CroppedBitmap cbit2 = new CroppedBitmap(img, new Int32Rect(10, 10, 200, 100));
-                //CroppedBitmap cbit3 = new CroppedBitmap(img, new Int32Rect(20, 20, 200, 100));
-                //CroppedBitmap cbit4 = new CroppedBitmap(img, new Int32Rect(30, 30, 200, 100));
-
-                //cb.Source = cbit1;
-                //cb2.Source = cbit2;
-                //cb3.Source = cbit3;
-                //cb4.Source = cbit4;
+            //        for (int y = 0; y < 4; y++)
+            //        {
+            //                    //JU STÖRRE BILD. DESTO HÖGRE SKALA OCH ANTAL PIXLAR I "Int32Rect"
+            //                cb = new CroppedBitmap(img, new Int32Rect(x * 200, y * 200, 200,200));
 
 
+            //            Random pix = new Random();
+            //            image[x, y].Source = cb;
+            //        }
+            //    }
 
-            }
+
+            //    ////BitmapImage bitmap = new BitmapImage();
+            //    ////bitmap.BeginInit();
+            //    //////bitmap.UriSource = new Uri(open_File.FileName);
+            //    ////bitmap.EndInit();
+
+            //    //// Create a CroppedBitmap from BitmapImage  
+            //    //CroppedBitmap cbit1 = new CroppedBitmap(img, new Int32Rect(0, 0, 250, 200));
+            //    //CroppedBitmap cbit2 = new CroppedBitmap(img, new Int32Rect(10, 10, 200, 100));
+            //    //CroppedBitmap cbit3 = new CroppedBitmap(img, new Int32Rect(20, 20, 200, 100));
+            //    //CroppedBitmap cbit4 = new CroppedBitmap(img, new Int32Rect(30, 30, 200, 100));
+
+            //    //cb.Source = cbit1;
+            //    //cb2.Source = cbit2;
+            //    //cb3.Source = cbit3;
+            //    //cb4.Source = cbit4;
+
+
+
+            //}
         }
 
         private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
@@ -169,12 +177,17 @@ namespace Puzzle_jigsaw
             Application.Current.Shutdown();
         }
 
-        private void clickOnFull_Image(object sender, MouseButtonEventArgs e)
-        {
-            OpenFileDialog open_File = new OpenFileDialog();
-            open_File.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
-            popupFullImageWindow.Show();
-        }
+        //private void clickOnFull_Image(object sender, MouseButtonEventArgs e)
+        //{
+        //    //OpenFileDialog open_File = new OpenFileDialog();
+        //    //open_File.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
+        //    popupFullImageWindow.Show();
+        //    Image img = new Image();
+        //    img.Source = new BitmapImage(new Uri("Image/Cute_Cat.jpg", UriKind.Relative));
+        //    img.Width = popupFullImageWindow.Width;
+        //    img.Height = popupFullImageWindow.Height;
+        //    popupFullImageWindow.FullImageImage.Source = img.Source;
+        //}
 
         private void Window_Loaded_1(object sender, RoutedEventArgs e)
         {
